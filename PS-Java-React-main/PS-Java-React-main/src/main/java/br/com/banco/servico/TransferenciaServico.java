cfd19cb38bbb78fb1fbd49844e80fbf8dcc59119
Estevao <pedro.cordova2002@gmail.com>
2022-12-21 20:09:41 -0300

    update modelMapperConfig para Mapeamento de DTO -> Entity

--- PS-Java-React-main/PS-Java-React-main/src/main/java/br/com/banco/servico/TransferenciaServico.java
+++ PS-Java-React-main/PS-Java-React-main/src/main/java/br/com/banco/servico/TransferenciaServico.java
@@ -2,14 +2,15 @@ package br.com.banco.servico;
 
 import br.com.banco.repositorio.TransferenciaRepositorio;
 import br.com.banco.servico.dto.TransferenciaDto;
-import br.com.banco.servico.mapper.TransferenciaMapper;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -17,16 +18,18 @@ import java.util.List;
 public class TransferenciaServico implements Serializable {
 
     private final TransferenciaRepositorio repositorio;
-    private final TransferenciaMapper mapper;
+    private final ModelMapper modelMapper;
 
     public List<TransferenciaDto> findByDate(LocalDateTime data) {
         var listTransferencia= repositorio.listTransferenciaDate(data);
-        return mapper.toDto(listTransferencia);
+        return listTransferencia.stream()
+                .map(r -> modelMapper.map(r, TransferenciaDto.class)).collect(Collectors.toList());
     }
 
     public List<TransferenciaDto> findByNomeOperador(String nome) {
         var listTransferencia= repositorio.listTransferenciaPorNomeOperadorTransacao(nome);
-        return mapper.toDto(listTransferencia);
+        return listTransferencia.stream()
+                .map(r -> modelMapper.map(r, TransferenciaDto.class)).collect(Collectors.toList());
     }
 
     public List<TransferenciaDto> findByNomeOperadorAndDate(String nome, LocalDateTime dataInic, LocalDateTime dataFim) {
@@ -34,7 +37,8 @@ public class TransferenciaServico implements Serializable {
             throw new RuntimeException("A data inicial deve ser anterior a data final");
         }
         var listTransferencia= repositorio.listTransferenciaTodosParametros(nome, dataInic, dataFim);
-        return mapper.toDto(listTransferencia);
+        return listTransferencia.stream().map(r ->
+                modelMapper.map(r, TransferenciaDto.class)).collect(Collectors.toList());
     }
 
 }
--- PS-Java-React-main/PS-Java-React-main/src/main/java/br/com/banco/servico/mapper/TransferenciaMapper.java
+++ PS-Java-React-main/PS-Java-React-main/src/main/java/br/com/banco/servico/mapper/TransferenciaMapper.java
@@ -3,8 +3,6 @@ package br.com.banco.servico.mapper;
 
 import br.com.banco.entidades.Transferencia;
 import br.com.banco.servico.dto.TransferenciaDto;
-import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "spring")
 public interface TransferenciaMapper extends EntityMapper<TransferenciaDto, Transferencia> {
 }
